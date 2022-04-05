53231fb5e56ad58211e3a045ada0200f5db2be97
Patricia Kihara <patricia.kihara@outlook.com>
2022-04-05 18:29:52 -0300

    Charts component created

---
+++ src/components/Charts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+class Charts extends React.Component {
+  render() {
+    return (
+      <div className="col-12 col-md-7">
+        <canvas id="barChart" width="400" height="400"></canvas>
+      </div>
+    );
+  }
+}
+
+export default Charts;
